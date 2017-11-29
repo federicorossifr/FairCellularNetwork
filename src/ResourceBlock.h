f13fb00b9a9592388e16ed819ac4e12b61da07b8
gioele95 <gioele95@users.noreply.github.com>
2017-11-29 10:07:23 +0100

    added setter and getter fun in RB.h

--- src/ResourceBlock.h
+++ src/ResourceBlock.h
@@ -15,6 +15,14 @@ public:
 	~ResourceBlock() {;};
 	int insertPacket(Packet* p);
 	Packet* popPacket();
+	int getSize(){return size;};
+	int getAvailable(){return available;};
+	int getUserID(){return userID;};
+	cQueue getPackets(){return packets;};
+	void setSize(int s){size=s;};
+    void setAvailable(int a){available=a;};
+    void setUserID(int id){userID=id;};
+    void setPackets(cQueue p){packets=p;};
 };
 
 #endif
