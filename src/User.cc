22cd120925a0730403454f2841c0dab15f8df790
Mona Lisa <bernardi.lb94@gmail.com>
2017-11-29 17:37:09 +0100

    I HAVE THE BATTERY

--- src/User.cc
+++ src/User.cc
@@ -26,51 +26,51 @@ void User::handleMessage(cMessage *msg)
     }
     else{
         Frame* frame = check_and_cast<Frame *>(msg);
-    	long previousMsgId = 0;
+    	long previousMsgId = -1;
     	Packet* fragmentMessage = NULL;
 
     	//scan the broadcast RBs frame arrived
     	for(int i=0; i<25; i++){
     		//check if the current RB contains packet that belong to me, otherwise skip it
     	    ResourceBlock* rb = frame->get_rbs(i);
-    	    if(rb->getUserID() == userID){
-    			Packet* p = NULL;
-    			while(p = check_and_cast<Packet *>(rb->popPacket())){
-    				//check if the current packet is completely content in the current RB
-    				if(!p->getFragment()){
-    					simtime_t responseTime = simTime() - p->getCreation();
-    					//TODO - save response time with signal
-    					delete p;
+    	    if(rb->getUserID() != userID)
+    	        continue;
+            Packet* p = NULL;
+            while(p = check_and_cast<Packet *>(rb->popPacket())){
+                //check if the current packet is completely content in the current RB
+                if(!p->getFragment()){
+                    simtime_t responseTime = simTime() - p->getCreation();
+                    //TODO - save response time with signal
+                    delete p;
 
-    					//check if the previous packet in the current RB was a "last fragment" of a packet bigger than user's RB size
-    					if(fragmentMessage != NULL){
-    						simtime_t responseTime = simTime() - fragmentMessage->creation;
-    						//TODO - save response time with signal
-    						delete fragmentMessage;
+                    //check if the previous packet in the current RB was a "last fragment" of a packet bigger than user's RB size
+                    if(fragmentMessage != NULL){
+                        simtime_t responseTime = simTime() - fragmentMessage->getCreation();
+                        //TODO - save response time with signal
+                        delete fragmentMessage;
 
-    						fragmentMessage = NULL;
-    						previousMsgId = 0;
-    					}
-    				}
-    				else{
-    					//the previous packet was all content in the RB
-    					if(previousMsgId == 0){
-    						previousMsgId = p.getId();
-    						fragmentMessage = p;
-    					}
-    					//check if this fragment is a fragment of the previous fragmented packet or a fragment of a new one
-    					else if(previousMsgId != p.getId()){
-    						simtime_t responseTime = simTime() - fragmentMessage->creation;
-    						//TODO - save response time with signal
-    						delete fragmentMessage;
+                        fragmentMessage = NULL;
+                        previousMsgId = -1;
+                    }
+                }
+                else{
+                    //the previous packet was all content in the RB
+                    if(previousMsgId == -1){
+                        previousMsgId = p->getId();
+                        fragmentMessage = p;
+                    }
+                    //check if this fragment is a fragment of the previous fragmented packet or a fragment of a new one
+                    else if(previousMsgId != p->getId()){
+                        simtime_t responseTime = simTime() - fragmentMessage->getCreation();
+                        //TODO - save response time with signal
+                        delete fragmentMessage;
 
-    						fragmentMessage = p;
-    						previousMsgId =  p.getId();
-    					}
-    				}
-    			}
-    		}
+                        fragmentMessage = p;
+                        previousMsgId =  p->getId();
+                    }
+                }
+            }
     	}
-
+    	delete frame;
     }
 }
